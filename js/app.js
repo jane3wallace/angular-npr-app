6416b0689257be724b99ebab42bb81ae72323c8c
jane3wallace <jane3wallace@gmail.com>
2016-03-29 23:14:39 -0400

    added npr link directive, foundation styles and custom css

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+$(document).foundation();
+
 var app = angular.module('nprApp', []),
 	apiKey = 'MDIzNDk0NjUyMDE0NTkyOTQyNTU3MDMzNQ000',
 	nprURL = 'http://api.npr.org/query?id=61&fields=relatedLink,title,byline,text,audio,image,pullQuote&output=JSON';
--- js/controllers/PlayerController.js
+++ js/controllers/PlayerController.js
@@ -1,22 +1,19 @@
 app.controller('PlayerController', ['$scope', '$http', function($scope, $http) {
 	var audio = document.createElement('audio');
 	$scope.audio = audio;
-	// audio.playing = false;
-	audio.src = 'http://pd.npr.org/npr-mp4/npr/sf/2013/07/20130726_sf_05.mp4?orgId=1&topicId=1032&ft=3&f=61';
-	audio.play();
-	// audio.play = function() {
-	// 	audio.play();
-	// 	audio.playing = true;
-	// };
-	// audio.stop = function() {
-	// 	audio.pause();
-	// 	audio.playing = false;
-	// };
-	// audio.addEventListener('ended', function() {
-	// 	$scope.$apply(function() {
-	// 		audio.stop()
-	// 	});
-	// });
+	
+	// audio.src = 'http://pd.npr.org/npr-mp4/npr/sf/2013/07/20130726_sf_05.mp4?orgId=1&topicId=1032&ft=3&f=61';
+	// audio.play();
+	
+	$scope.play = function(program) {
+		if ($scope.playing) $scope.audio.pause();
+		var url = program.audio[0].format.mp4.$text;
+		audio.src = url;
+		audio.play();
+		// Store the state of the player as playing
+		$scope.playing = true;
+	}
+	
 	$http({
 		method: 'JSONP',
 		url: nprURL + '&apiKey=' + apiKey + '&callback=JSON_CALLBACK'
